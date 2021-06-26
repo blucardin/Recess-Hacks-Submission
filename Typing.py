0945894cfdbc276a468f7afc8917d98cb5b7e28a
Sam Cardin <blucardin@gmail.com>
2021-06-26 14:58:16 -0400

    Currently not working, added database feature, but having difficulty with implementing it into modules

--- Typing.py
+++ Typing.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import discord
 import random
 import time
+import leaderboard
 
 def run(bot): 
     
---
+++ leaderboard.py
@@ -0,0 +1,48 @@
+
+playersR = open("players.txt", "rt")
+scoresR = open("scores.txt", "rt")
+
+listplayers = playersR.read().split(" ")
+listsscore = scoresR.read().split(" ")
+
+print(listplayers)
+print(listsscore)
+
+playersR.close()
+scoresR.close()
+
+def retrivescore(player):
+    if player in listplayers: 
+        index = listplayers.index(player) 
+        return listsscore[index]
+    else:
+        return "Player not in database"
+
+
+def add_to_score(player, amount):
+    if player in listplayers: 
+        index = listplayers.index(player) 
+        listsscore[index] = str(int(listsscore[index]) + int(amount))
+
+    else:
+        listplayers.append(player)
+        listsscore.append(amount)
+
+    separator = " "
+    newscorelist = separator.join(listsscore)
+    newplayerlist = separator.join(listplayers)
+    print(newscorelist)
+    print(newplayerlist)
+
+    playersW = open("players.txt", "wt")
+    scoresW = open("scores.txt", "wt")
+
+    playersW.write(newplayerlist)
+    scoresW.write(newscorelist)
+
+    playersW.close()
+    scoresW.close()
+    
+
+
+
